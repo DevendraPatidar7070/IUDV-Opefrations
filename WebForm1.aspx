51a51384fd63020d3d29454dfc55198552a3ca77
DevendraPatidar7070 <77269809+DevendraPatidar7070@users.noreply.github.com>
2022-01-30 18:39:27 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,87 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="DataOperations.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+    <link href="Content/bootstrap-reboot.min.css" rel="stylesheet" />
+    <script src="Scripts/bootstrap.min.js"></script>
+    <script src="Scripts/bootstrap.bundle.min.js"></script>
+</head>
+<body style="height: 440px">
+    <form id="form1" runat="server">
+
+        <div>
+            <table>
+                <tr>
+                    <td>Enter Your First Name: </td>
+                    <td>
+                        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox> </td>
+                </tr>
+                <tr>
+                    <td>Enter Your Last Name: </td>
+                    <td>
+                        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox> </td>
+                </tr>
+                <tr>
+                    <td>Enter Your Mobile No: </td>
+                    <td>
+                        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox> </td>
+                </tr>
+                <tr>
+                    <td> Enter DOB : </td>
+                    <td> <asp:TextBox ID="TextBox4" runat="server" PlaceHolder="YYYY-MM-DD" TextMode="Date"></asp:TextBox> </td>
+                </tr>
+                <tr>
+                    <td> Select Gender : </td>
+                    <td>
+                        <asp:RadioButtonList ID="RadioButtonList1" runat="server">
+                            <asp:ListItem>Male</asp:ListItem>
+                            <asp:ListItem>Female</asp:ListItem>
+                            <asp:ListItem>Other</asp:ListItem>
+                        </asp:RadioButtonList>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Enter Email :</td>
+                    <td>
+                        <asp:TextBox ID="TextBox5" runat="server" TextMode ="Email"></asp:TextBox> </td>
+                </tr>
+                <tr>
+                    <td>Enter Username: </td>
+                    <td> <asp:TextBox ID="TextBox6" runat="server"></asp:TextBox> </td>
+                </tr>
+                <tr>
+                    <td>Enter Password: </td>
+                    <td>
+                        <asp:TextBox ID="TextBox7" runat="server" TextMode="Password"></asp:TextBox></td>
+                </tr>
+
+                <tr colspan="2">
+                    <td>
+                        <asp:Button ID="Button1" runat="server" Text="Insert" OnClick="Button1_Click" />
+                        <asp:Button ID="Button2" runat="server" Text="Update" OnClick="Button2_Click" />
+                    </td>
+                    
+                    <td>
+                        <asp:Button ID="Button3" runat="server" Text="Delete" OnClick="Button3_Click" class="btn btn-link" />
+                        <asp:Button ID="Button4" runat="server" Text="View" OnClick="Button4_Click" />
+                    </td>
+                </tr>         
+            </table>
+            <br />
+            Update Using Mobile : <asp:TextBox ID="TextBox8" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Delete Using FirstName:
+            <asp:TextBox ID="TextBox9" runat="server"></asp:TextBox>
+            <br />
+            <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+        </div>
+
+        </form>
+    </body>
+</html>
+        
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataOperations
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-1AT9VT2\SQLEXPRESS;Initial Catalog=Webform1;Integrated Security=True");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.Open();
+            
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            data_ins();
+            disp_data();
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            update_data();
+            disp_data();
+        }
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            delete_data();
+            disp_data();
+        }
+        protected void Button4_Click(object sender, EventArgs e)
+        {  
+            disp_data();
+        } 
+        public void empty()
+        {
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            TextBox4.Text = "";
+            TextBox5.Text = "";
+            TextBox6.Text = "";
+            TextBox7.Text = "";
+            RadioButtonList1.Items[0].Selected = false;
+            RadioButtonList1.Items[1].Selected = false;
+            RadioButtonList1.Items[2].Selected = false;
+        }
+        public void disp_data()
+        {
+            if (con.State == ConnectionState.Open)
+            { con.Close(); }
+
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from information";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            
+        }
+        public void data_ins()
+        {
+            if (con.State == ConnectionState.Open)
+            { con.Close(); }
+
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Insert into information values('" + TextBox1.Text + "','" + TextBox2.Text + "','"+ TextBox3.Text +"','" + TextBox4.Text + "','" + RadioButtonList1.Text + "','" + TextBox5.Text + "','" + TextBox6.Text + "','" + TextBox7.Text + "')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            empty();
+        }
+        public void update_data()
+        {   
+            if (con.State == ConnectionState.Open)
+            {  con.Close(); }
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "update information set [firstname]='"+ TextBox1.Text + "',[lastname]='" + TextBox2.Text +"',[mobile]='" + TextBox3.Text + "',[dob]='" + TextBox4.Text + "',[gender]='" + RadioButtonList1.Text + "',[email]= '" + TextBox5.Text + "',[username]='" + TextBox6.Text + "',[password]='" + TextBox7.Text + "' where [mobile] ='"+TextBox8.Text+"' ";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            TextBox8.Text = "";  
+        }
+        public void delete_data()
+        {
+            if (con.State == ConnectionState.Open)
+            { con.Close(); }
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "delete from information where [firstname]='"+TextBox9.Text+"' ";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            TextBox9.Text = "";
+        }
+
+      
+    }
+}
